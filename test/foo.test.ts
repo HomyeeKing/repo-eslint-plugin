155071f0c51585ca19d4ee563dda711dc490d578
HomyeeKing <HomyeeKing@gmail.com>
2023-10-25 10:14:18 +0800

    feat: save

--- test/foo.test.ts
+++ test/foo.test.ts
@@ -1,9 +1,14 @@
-import * as rdt from 'react-dev-utils/openBrowser';
+import ob from 'react-dev-utils/openBrowser';
 import { expect, it, vi } from 'vitest';
 
-it('test', () => {
+vi.mock('react-dev-utils/openBrowser', () => {
+  return {
+    default: vi.fn(),
+  };
+});
 
-  const spy = vi.spyOn(rdt,'default','get');
-  rdt.default('https://www.taobao.com');
-  // expect(spy).toHaveBeenCalledTimes(1);
+it('test', () => {
+  ob('https://www.google.com');
+  expect(ob).toHaveBeenCalledTimes(1);
+  expect(ob).toHaveBeenCalledWith('https://www.google.com');
 });
